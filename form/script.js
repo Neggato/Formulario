54ba75e0dcc5a7134854934c78674cd3fd3464c4
jotave <jvfelix86@gmail.com>
2024-08-29 20:10:25 -0300

    Final

---
+++ form/script.js
@@ -0,0 +1,109 @@
+
+const form = document.getElementById("form");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const passowordConfirmation = document.getElementById("password-confirmation");
+
+form.addEventListener("submit", (event) =>{
+    event.preventDefault();
+
+    checkForm();
+
+})
+
+email.addEventListener("blur", () => {
+    checkInputEmail();
+  })
+  
+  
+  username.addEventListener("blur", () => {
+    checkInputUsername();
+  })
+  
+
+function checkForm(){
+    checkInputUsername();
+    checkInputEmail();
+    checkInputPassword();
+    checkInputPasswordConfirmation();
+  
+    const formItems = form.querySelectorAll(".form-content")
+  
+    const isValid = [...formItems].every( (item) => {
+      return item.className === "form-content"
+    });
+  
+    if(isValid){
+      alert("CADASTRADO COM SUCESSO!")
+    }
+  
+  }
+
+
+function checkInputEmail(){
+    const emailValue = email.value;
+
+    if(emailValue === ""){
+        errorInput(email, "O email é obrigatório!")
+    }else{
+        const formItem = email.parentElement;
+            formItem.className = "form-content"
+    }
+}
+
+
+function checkInputPassword(){
+    const passwordValue = password.value;
+  
+    if(passwordValue === ""){
+      errorInput(password, "A senha é obrigatória.")
+    }else if(passwordValue.length < 8){
+      errorInput(password, "A senha precisa ter no mínimo 8 caracteres.")
+    }else{
+      const formItem = password.parentElement;
+      formItem.className = "form-content"
+    }
+}
+
+function checkInputPasswordConfirmation(){
+    const passwordValue = password.value;
+    const confirmationPasswordValue = passowordConfirmation.value;
+  
+    if(confirmationPasswordValue === ""){
+      errorInput(passowordConfirmation, "A confirmação de senha é obrigatória.")
+    }else if(confirmationPasswordValue !== passwordValue){
+      errorInput(passowordConfirmation, "As senhas não são iguais!")
+    }else{
+      const formItem = passowordConfirmation.parentElement;
+      formItem.className = "form-content"
+    }
+}
+
+function checkForm(){
+    checkInputUsername();
+    checkInputEmail();
+    checkInputPassword();
+    checkInputPasswordConfirmation();
+
+    const formItems = form.querySelectorAll(".forn-content")
+
+    const isValid = [...formItems.every].every ( (item) => {
+        return item.className === "form-content" 
+    });
+
+    if(isValid){
+        alert("CADASTRADO COM SUCESSO!")
+      }
+
+}
+
+function errorInput(input, message){
+    const formItem = input.parentElement;
+    const textMessage = formItem.querySelector("a")
+
+    textMessage.innerText = message;
+
+    formItem.className = "form-content error"
+
+}
